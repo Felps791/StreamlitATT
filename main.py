9eb33c6b77df6ed0ef74ad1793b3b6fb88feadf4
Felps791 <felipedtm791@hotmail.com>
2024-10-01 12:04:45 +0000

    main,py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def calcIMC();
+    st.title("Calculadora de IMC")
+    st.latex(r"\text{IMC} = \frac{\text{Peso}}{\text{Altura}^2}")
+    st.image("IMCTable.png")
+")
+
+st.title("Atividade 1 - Streamlit")
+
+calcIMC()
