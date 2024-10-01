56c8ddb340021f989617f3d4c7d42067671573ed
Felps791 <felipedtm791@hotmail.com>
2024-10-01 12:30:27 +0000

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 def calcIMC():
     st.title("Calculadora de IMC")
     st.latex(r"\text{IMC} = \frac{\text{Peso}}{\text{Altura}^2}")
-    st.image("/workspaces/StreamlitATT/IMCTable.png")
+    st.image("IMCTable.png")
 
 
 st.title("Atividade 1 - Streamlit")
