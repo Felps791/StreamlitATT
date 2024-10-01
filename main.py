4a37f27a955b1a7b1c4d935d3ee21bac0d379622
Felps791 <felipedtm791@hotmail.com>
2024-10-01 12:05:20 +0000

    main,py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def calcIMC();
     st.title("Calculadora de IMC")
     st.latex(r"\text{IMC} = \frac{\text{Peso}}{\text{Altura}^2}")
     st.image("IMCTable.png")
-")
+
 
 st.title("Atividade 1 - Streamlit")
 
